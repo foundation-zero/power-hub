b0b41cc77f9212e841fc8979ab8dbc35a2a71600
Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
2024-02-13 09:27:40 +0100

    Add heater control logic (#1)

--- tests/test_control.py
+++ tests/test_control.py
@@ -1,5 +1,29 @@
-from energy_box_control.control import control
+from pytest import approx
+from energy_box_control.logic import ControlState, control
+
+from energy_box_control.simulation import Boiler, BoilerState, Network, State, simulate
 
 
-def test_control():
-    assert control((), ()) == ((), ())
+def run(network, state, control_state, times):
+    for _ in range(times):
+        sensors = state.sensors()
+        new_control_state, control_values = control(network, control_state, sensors)
+        new_state = simulate(network, state, control_values)
+
+        control_state = new_control_state
+        state = new_state
+
+    return state, control_state
+
+
+def test_heater():
+    network = Network(Boiler(10, 1))
+    state = State(BoilerState(0))
+    control_state = ControlState(50)
+
+    state_1, new_control_state = run(network, state, control_state, 1000)
+
+    assert state_1.boiler_state.temperature == approx(control_state.boiler_setpoint)
+
+    state_2, new_control_state = run(network, state, control_state, 1000)
+    assert state_2.boiler_state.temperature == state_2.boiler_state.temperature
