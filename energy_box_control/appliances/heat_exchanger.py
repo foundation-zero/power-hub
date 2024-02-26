25b48e483049678b2d1d3e2dc34b271d5968abe7
aljevandam <19215316+aljevandam@users.noreply.github.com>
2024-02-26 13:10:14 +0100

    Implement heat exchanger simulation (#15)
    * Add simulate for heat exchanger
    
    * Add test
    
    * Process feedback

--- energy_box_control/appliances/heat_exchanger.py
+++ energy_box_control/appliances/heat_exchanger.py
@@ -3,13 +3,9 @@ from energy_box_control.appliances.base import (
     Appliance,
     ApplianceControl,
     ApplianceState,
+    ConnectionState,
     Port,
 )
-
-
-@dataclass(eq=True, frozen=True)
-class HeatExchangerState(ApplianceState):
-    pass
 
 
 class HeatExchangerPort(Port):
@@ -19,5 +15,40 @@ class HeatExchangerPort(Port):
     B_OUT = "B_OUT"
 
 
-class HeatExchanger(Appliance[HeatExchangerState, ApplianceControl, HeatExchangerPort]):
-    pass
+@dataclass(frozen=True, eq=True)
+class HeatExchanger(Appliance[ApplianceState, ApplianceControl, HeatExchangerPort]):
+    specific_heat_capacity_A: float
+    specific_heat_capacity_B: float
+
+    # assuming a perfect heat exchange, reaching thermal equilibrium in every time step
+    def simulate(
+        self,
+        inputs: dict[HeatExchangerPort, ConnectionState],
+        previous_state: ApplianceState,
+        control: ApplianceControl,
+    ) -> tuple[ApplianceState, dict[HeatExchangerPort, ConnectionState]]:
+
+        heat_A = (
+            inputs[HeatExchangerPort.A_IN].flow
+            * inputs[HeatExchangerPort.A_IN].temperature
+            * self.specific_heat_capacity_A
+        )
+        heat_B = (
+            inputs[HeatExchangerPort.B_IN].flow
+            * inputs[HeatExchangerPort.B_IN].temperature
+            * self.specific_heat_capacity_B
+        )
+
+        equilibrium_temperature = (heat_A + heat_B) / (
+            inputs[HeatExchangerPort.A_IN].flow * self.specific_heat_capacity_A
+            + inputs[HeatExchangerPort.B_IN].flow * self.specific_heat_capacity_B
+        )
+
+        return ApplianceState(), {
+            HeatExchangerPort.A_OUT: ConnectionState(
+                inputs[HeatExchangerPort.A_IN].flow, equilibrium_temperature
+            ),
+            HeatExchangerPort.B_OUT: ConnectionState(
+                inputs[HeatExchangerPort.B_IN].flow, equilibrium_temperature
+            ),
+        }
---
+++ tests/test_heat_exchanger.py
@@ -0,0 +1,41 @@
+from energy_box_control.appliances.base import (
+    ApplianceControl,
+    ApplianceState,
+    ConnectionState,
+)
+from energy_box_control.appliances.heat_exchanger import (
+    HeatExchanger,
+    HeatExchangerPort,
+)
+
+
+def test_heat_exchanger_equal_flow():
+    exchanger = HeatExchanger(2, 1)
+
+    _, output = exchanger.simulate(
+        {
+            HeatExchangerPort.A_IN: ConnectionState(1, 0),
+            HeatExchangerPort.B_IN: ConnectionState(1, 90),
+        },
+        ApplianceState(),
+        ApplianceControl(),
+    )
+
+    assert output[HeatExchangerPort.A_OUT] == ConnectionState(1, 30)
+    assert output[HeatExchangerPort.B_OUT] == ConnectionState(1, 30)
+
+
+def test_heat_exchanger_equal_capacity():
+    exchanger = HeatExchanger(1, 1)
+
+    _, output = exchanger.simulate(
+        {
+            HeatExchangerPort.A_IN: ConnectionState(3, 0),
+            HeatExchangerPort.B_IN: ConnectionState(1, 100),
+        },
+        ApplianceState(),
+        ApplianceControl(),
+    )
+
+    assert output[HeatExchangerPort.A_OUT] == ConnectionState(3, 25)
+    assert output[HeatExchangerPort.B_OUT] == ConnectionState(1, 25)
