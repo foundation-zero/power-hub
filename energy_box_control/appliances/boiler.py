201d26e7ebc6645b515600a2794d81eaf3b6c60f
Alje <alje.vandam@foundationzero.org>
2024-02-26 13:01:31 +0100

    Add fill to boiler

--- energy_box_control/appliances/boiler.py
+++ energy_box_control/appliances/boiler.py
@@ -17,7 +17,8 @@ class BoilerState(ApplianceState):
 class BoilerPort(Port):
     HEAT_EXCHANGE_IN = "HEAT_EXCHANGE_IN"
     HEAT_EXCHANGE_OUT = "HEAT_EXCHANGE_OUT"
-
+    FILL_IN = "FILL_IN"
+    FILL_OUT = "FILL_OUT"
 
 @dataclass(frozen=True, eq=True)
 class BoilerControl(ApplianceControl):
@@ -26,17 +27,12 @@ class BoilerControl(ApplianceControl):
 
 @dataclass(frozen=True, eq=True)
 class Boiler(Appliance[BoilerState, BoilerControl, BoilerPort]):
-    heat_capacity: float  # TODO: split into volume & specific heat capacity
-    heater_power: float  # TODO: power going into water. Define efficiency later
+    heat_capacity_tank: float
+    heater_power: float
     heat_loss: float
-    specific_heat_capacity_input: float  # J / l K #TODO: this should probably go into Connection or ConnectionState (as it varies with temp), and be in J / kg K
-
-    # TODO: make temp dependent
-    # heat_capacity_pipefluid = 3790  # J / (kg K) at 20C
-    # density_pipefluid = 1.009  # kg / l at 20C
-    # heat_capacity_water = 4184  # J / (kg K) at 20C
-    # density_water = 0.997  # kg / l at 20C
-
+    specific_heat_capacity_exchange: float  # J / l K
+    specific_heat_capacity_fill: float # J / l K
+    
     def simulate(
         self,
         inputs: dict[BoilerPort, ConnectionState],
@@ -44,21 +40,23 @@ class Boiler(Appliance[BoilerState, BoilerControl, BoilerPort]):
         control: BoilerControl,
     ) -> Tuple[BoilerState, dict[BoilerPort, ConnectionState]]:
 
-        input = inputs[BoilerPort.HEAT_EXCHANGE_IN]
-
-        # assuming a perfect heat exchange, reaching thermal equilibrium in every time step #TODO: properly simulate heat exchange, which would require transfer coefficient or something similar
+        # assuming a perfect heat exchange, reaching thermal equilibrium in every time step
         element_heat = self.heater_power * control.heater_on
-        boiler_heat = self.heat_capacity * previous_state.temperature
-        input_heat = input.flow * self.specific_heat_capacity_input * input.temperature
+        tank_heat = self.heat_capacity_tank * previous_state.temperature
+        exchange_heat = inputs[BoilerPort.HEAT_EXCHANGE_IN].flow * self.specific_heat_capacity_exchange * inputs[BoilerPort.HEAT_EXCHANGE_IN].temperature
+        fill_heat = inputs[BoilerPort.FILL_IN].flow * self.specific_heat_capacity_fill * inputs[BoilerPort.FILL_IN].temperature
 
         equilibrium_temperature = (
-            element_heat + boiler_heat + input_heat - self.heat_loss
-        ) / (self.heat_capacity + input.flow * self.specific_heat_capacity_input)
+            element_heat + tank_heat + exchange_heat + fill_heat - self.heat_loss
+        ) / (self.heat_capacity_tank + inputs[BoilerPort.HEAT_EXCHANGE_IN].flow * self.specific_heat_capacity_exchange + inputs[BoilerPort.FILL_IN].flow * self.specific_heat_capacity_fill)
 
         return BoilerState(
             temperature=equilibrium_temperature,
         ), {
             BoilerPort.HEAT_EXCHANGE_OUT: ConnectionState(
-                input.flow, equilibrium_temperature
+                inputs[BoilerPort.HEAT_EXCHANGE_OUT].flow, equilibrium_temperature
+            ),
+            BoilerPort.FILL_OUT: ConnectionState(
+                inputs[BoilerPort.FILL_OUT].flow, equilibrium_temperature
             )
         }
