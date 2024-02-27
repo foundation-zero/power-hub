643d69c3b81f72c8b6f67a41ea2d413444777534
Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
2024-02-27 14:11:22 +0100

    Add heat pipes simulation

--- energy_box_control/appliances/heat_pipes.py
+++ energy_box_control/appliances/heat_pipes.py
@@ -3,6 +3,7 @@ from energy_box_control.appliances.base import (
     Appliance,
     ApplianceControl,
     ApplianceState,
+    ConnectionState,
     Port,
 )
 
@@ -17,5 +18,25 @@ class HeatPipesPort(Port):
     OUT = "OUT"
 
 
+@dataclass(frozen=True, eq=True)
 class HeatPipes(Appliance[HeatPipesState, ApplianceControl, HeatPipesPort]):
-    pass
+    power: float
+    max_temp: float
+    specific_heat: float
+
+    def simulate(
+        self,
+        inputs: dict[HeatPipesPort, ConnectionState],
+        previous_state: HeatPipesState,
+        control: ApplianceControl,
+    ) -> tuple[HeatPipesState, dict[HeatPipesPort, ConnectionState]]:
+        input = inputs[HeatPipesPort.IN]
+
+        if input.temperature >= self.max_temp:
+            return previous_state, {HeatPipesPort.OUT: input}
+        else:
+            dT = self.power / (input.flow * self.specific_heat)
+            new_temp = min(input.temperature + dT, self.max_temp)
+            return previous_state, {
+                HeatPipesPort.OUT: ConnectionState(input.flow, new_temp)
+            }
