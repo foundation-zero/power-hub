c855b7675d544ffee77b4539b304bc8130586995
Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
2024-02-27 14:11:22 +0100

    Add heat pipes simulation

---
+++ tests/test_heat_pipes.py
@@ -0,0 +1,35 @@
+from pytest import fixture
+from energy_box_control.appliances.base import ApplianceControl, ConnectionState
+from energy_box_control.appliances import HeatPipes, HeatPipesState, HeatPipesPort
+
+
+@fixture
+def heat_pipes():
+    return HeatPipes(50, 100, 1)
+
+
+def test_heat(heat_pipes):
+    _, outputs = heat_pipes.simulate(
+        {
+            HeatPipesPort.IN: ConnectionState(1, 0),
+        },
+        HeatPipesState(),
+        ApplianceControl(),
+    )
+    assert (
+        outputs[HeatPipesPort.OUT].temperature
+        == heat_pipes.power / heat_pipes.specific_heat
+    )
+    assert outputs[HeatPipesPort.OUT].flow == 1
+
+
+def test_maintain_max_temp(heat_pipes):
+    _, outputs = heat_pipes.simulate(
+        {
+            HeatPipesPort.IN: ConnectionState(1, heat_pipes.max_temp),
+        },
+        HeatPipesState(),
+        ApplianceControl(),
+    )
+    assert outputs[HeatPipesPort.OUT].temperature == heat_pipes.max_temp
+    assert outputs[HeatPipesPort.OUT].flow == 1
