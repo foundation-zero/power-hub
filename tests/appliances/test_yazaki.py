f46d3eab5e2c7e3702ed717c6be07f83f3c1e0dc
sietse-zero <127860207+sietse-zero@users.noreply.github.com>
2024-03-27 16:44:06 +0100

    Add yazaki out of range logic (#34)

---
+++ tests/appliances/test_yazaki.py
@@ -0,0 +1,56 @@
+from pytest import approx
+import pytest
+from energy_box_control.appliances.base import ConnectionState
+from energy_box_control.appliances.source import Source
+from energy_box_control.appliances.yazaki import (
+    Yazaki,
+    YazakiControl,
+    YazakiPort,
+    YazakiState,
+)
+from energy_box_control.networks import YazakiNetwork
+import logging
+
+
+def test_yazaki():
+    network = YazakiNetwork(
+        Source(1.2, 88), Source(2.55, 31), Source(0.77, 17.6), Yazaki(4184, 4184, 4184)
+    )
+
+    control = network.control(network.yazaki).value(YazakiControl()).build()
+
+    state_1 = network.simulate(network.initial_state(), control)
+
+    assert state_1.connection(network.yazaki, YazakiPort.HOT_OUT).temperature == approx(
+        83, abs=1
+    )
+    assert state_1.connection(
+        network.yazaki, YazakiPort.COOLING_OUT
+    ).temperature == approx(35, abs=1)
+    assert state_1.connection(
+        network.yazaki, YazakiPort.CHILLED_OUT
+    ).temperature == approx(12.5, abs=1)
+
+
+def test_yazaki_outside_ref_values(caplog):
+    yazaki = Yazaki(4184, 4184, 4184)
+    initial_yazaki_state = YazakiState(efficiency=1)
+    yazaki_control = YazakiControl()
+    hot_in = 15
+    chilled_in = 0
+    cooling_in = 0
+    connections = {
+        YazakiPort.HOT_IN: ConnectionState(1, hot_in),
+        YazakiPort.CHILLED_IN: ConnectionState(1, chilled_in),
+        YazakiPort.COOLING_IN: ConnectionState(1, cooling_in),
+    }
+
+    _, outputs = yazaki.simulate(connections, initial_yazaki_state, yazaki_control)
+    caplog.set_level(logging.WARNING)
+    assert (
+        f"Hot in temperature of {hot_in} outside of hot reference temperatures. All values are passed through without change"
+        in caplog.text
+    )
+    assert outputs[YazakiPort.HOT_OUT].temperature == hot_in
+    assert outputs[YazakiPort.CHILLED_OUT].temperature == chilled_in
+    assert outputs[YazakiPort.COOLING_OUT].temperature == cooling_in
