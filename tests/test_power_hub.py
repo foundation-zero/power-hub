8a4fa665a4ec4f04225461ffaf06cd4f696d6df5
sietse-zero <127860207+sietse-zero@users.noreply.github.com>
2024-03-27 14:55:19 +0100

    Add test to assert exception in simulation (#32)

--- tests/test_power_hub.py
+++ tests/test_power_hub.py
@@ -1,6 +1,9 @@
+import pytest
 from energy_box_control.appliances import BoilerControl
 from energy_box_control.appliances.switch_pump import SwitchPumpControl
+from energy_box_control.networks import ControlState
 from energy_box_control.power_hub import PowerHub
+from dataclasses import dataclass
 
 
 def test_power_hub_simulation():
@@ -24,3 +27,35 @@ def test_power_hub_simulation():
         .value(SwitchPumpControl(on=True))
         .build(),
     )
+
+
+@dataclass(frozen=True, eq=True)
+class SimulationSuccess:
+    pass
+
+
+@dataclass(frozen=True, eq=True)
+class SimulationFailure:
+    exception: Exception
+    step: int
+
+
+def run_simulation():
+    power_hub = PowerHub.example_power_hub()
+    state = PowerHub.example_initial_state(power_hub)
+    control_state = ControlState(50)
+    min_max_temperature = (-500, 500)
+    for i in range(0, 500):
+        new_control_state, control_values = power_hub.regulate(control_state)
+        try:
+            new_state = power_hub.simulate(state, control_values, min_max_temperature)
+        except Exception as e:
+            return SimulationFailure(e, i)
+        control_state = new_control_state
+        state = new_state
+    return SimulationSuccess()
+
+
+@pytest.mark.skip
+def test_max_temperatures():
+    assert run_simulation() == SimulationSuccess()
