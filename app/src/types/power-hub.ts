45644261ef3343368422e02416e12ba4f529cbcc
David Hoevers <david@manklavier.nl>
2024-04-09 19:57:15 +0200

    Refactor app to updated api spec (#55)

--- app/src/types/power-hub.ts
+++ app/src/types/power-hub.ts
@@ -1,153 +1,54 @@
+import type { CamelCase } from "type-fest";
+
+type Port<PortName extends string = ""> = {
+  [key in CamelCase<`${PortName}${"InputTemperature" | "OutputTemperature" | "Flow"}`>]: number;
+};
+
 export interface Valve {
   position: number;
 }
 
-export interface Reservoir {
-  temperature: number;
-}
+export type Yazaki = Port<"hot"> &
+  Port<"cooling"> &
+  Port<"chilled"> & {
+    efficiency: number;
+  };
 
-export interface Port {
-  flow: number;
-  temperature: number;
-}
+export type PCM = Port<"charge"> &
+  Port<"discharge"> & {
+    temperature: number;
+    stateOfCharge: number;
+  };
 
-export interface Yazaki {
-  efficiency: number;
-}
+export type Chiller = Port<"cooling"> & Port<"chilled">;
 
-export interface PCM {
-  stateOfCharge: number;
-  temperature: number;
-}
+export type HeatPipes = Port & {
+  ambientTemperature: number;
+  globalIrradiance: number;
+};
 
-export interface AppliancesTree {
-  chillerSwitchValve: Valve;
-  chillerWasteBypassValve: Valve;
-  coldReservoir: Reservoir;
+export type Reservoir = Port<"heat"> &
+  Port<"fill"> & {
+    temperature: number;
+  };
+
+export interface SensorsTree {
+  heatPipes: HeatPipes;
   heatPipesValve: Valve;
+  hotReservoirPCMValve: Valve;
   hotReservoir: Reservoir;
-  hotReservoirPcmValve: Valve;
   pcm: PCM;
+  yazakiHotBypassValve: Valve;
+  yazaki: Yazaki;
+  chiller: Chiller;
+  chillerSwitchValve: Valve;
+  coldReservoir: Reservoir;
+  yazakiWasteBypassValve: Valve;
   preheatBypassValve: Valve;
   preheatReservoir: Reservoir;
   wasteSwitchValve: Valve;
-  yazaki: Yazaki;
-  yazakiWasteBypassValve: Valve;
-}
-
-export interface ConnectionsTree {
-  chillMix: {
-    a: Port;
-    b: Port;
-  };
-  chiller: {
-    chilledIn: Port;
-    chilledOut: Port;
-    coolingIn: Port;
-    coolingOut: Port;
-  };
-  chillerSwitchValve: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  chillerWasteBypassValve: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  chillerWasteMix: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  coldReservoir: {
-    heatExchangeOut: Port;
-  };
-  freshWaterSource: {
-    output: Port;
-  };
-  heatPipes: {
-    out: Port;
-  };
-  heatPipesMix: {
-    a: Port;
-    b: Port;
-  };
-  heatPipesValve: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  hotMix: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  hotReservoir: {
-    fillIn: Port;
-    heatExchangeIn: Port;
-    heatExchangeOut: Port;
-  };
-  hotReservoirPcmValve: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  outboard_exchange: {
-    aIn: Port;
-    bIn: Port;
-  };
-  outboardSource: {
-    output: Port;
-  };
-  pcm: {
-    chargeIn: Port;
-    chargeOut: Port;
-    dischargeOut: Port;
-  };
-  preheatBypassValve: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  preheatMix: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  preheatReservoir: {
-    fillIn: Port;
-    fillOut: Port;
-    heatExchangeIn: Port;
-    heatExchangeOut: Port;
-  };
-  wasteMix: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  wasteSwitchValve: {
-    a: Port;
-    b: Port;
-  };
-  yazaki: {
-    chilledIn: Port;
-    chilledOut: Port;
-    coolingIn: Port;
-    coolingOut: Port;
-    hotIn: Port;
-  };
-  yazakiWasteBypassValve: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
-  yazakiWasteMix: {
-    a: Port;
-    ab: Port;
-    b: Port;
-  };
+  hotReservoirPcmValve: Valve;
+  chillerWasteBypassValve: Valve;
 }
 
 export interface ComputedTree {
@@ -157,7 +58,5 @@ export interface ComputedTree {
 }
 
 export interface Tree {
-  connections: ConnectionsTree;
-  appliances: AppliancesTree;
-  computed: ComputedTree;
+  applianceSensors: SensorsTree;
 }
