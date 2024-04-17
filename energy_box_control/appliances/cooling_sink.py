c37f54e27827762d124c8488488cc75b98d7aacf
aljevandam <19215316+aljevandam@users.noreply.github.com>
2024-04-17 17:23:29 +0200

    Add cooling demand and extra pumps (#72)
    * Add freshwater pump and fix outboard source
    
    * Add cooling sink
    
    * Add cooling demand schedule
    
    * Update control to include fresh water and cooling demand pump
    
    * Add missing argument
    
    * Update cooling sink

---
+++ energy_box_control/appliances/cooling_sink.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from energy_box_control.appliances.base import (
+    Appliance,
+    ApplianceState,
+    ConnectionState,
+    Port,
+)
+from energy_box_control.schedules import Schedule
+from energy_box_control.time import ProcessTime
+from energy_box_control.units import JoulePerLiterKelvin, Watt
+
+
+class CoolingSinkPort(Port):
+    INPUT = "input"
+    OUTPUT = "output"
+
+
+@dataclass(frozen=True, eq=True)
+class CoolingSink(Appliance[ApplianceState, None, CoolingSinkPort]):
+    specific_heat_capacity: JoulePerLiterKelvin
+    cooling_demand_schedule: Schedule[Watt]
+
+    def simulate(
+        self,
+        inputs: dict[CoolingSinkPort, ConnectionState],
+        previous_state: ApplianceState,
+        control: None,
+        simulation_time: ProcessTime,
+    ) -> tuple[ApplianceState, dict[CoolingSinkPort, ConnectionState]]:
+
+        output_temperature = (
+            inputs[CoolingSinkPort.INPUT].temperature
+            + (
+                self.cooling_demand_schedule.at(simulation_time)
+                / (inputs[CoolingSinkPort.INPUT].flow * self.specific_heat_capacity)
+            )
+            if inputs[CoolingSinkPort.INPUT].flow > 0
+            else inputs[CoolingSinkPort.INPUT].temperature
+        )
+
+        return ApplianceState(), {
+            CoolingSinkPort.OUTPUT: ConnectionState(
+                inputs[CoolingSinkPort.INPUT].flow, output_temperature
+            )
+        }
