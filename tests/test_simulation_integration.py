d7687676229153a13c3cbc9adc551977530c0cd8
Sietse Huisman <sietse.huisman@blinkcv.com>
2024-05-01 13:55:21 +0200

    Process feedback PR #87

--- tests/test_simulation_integration.py
+++ tests/test_simulation_integration.py
@@ -1,65 +1,16 @@
-import asyncio
 from http import HTTPStatus
 import json
 import os
-import sys
 from dotenv import load_dotenv
 import pytest
 import requests
-
-from energy_box_control.api.api import (
-    build_get_values_query,
-    execute_influx_query,
-    get_influx_client,
-)
-
-from influxdb_client.client.influxdb_client_async import InfluxDBClientAsync
+from tests.test_api_integration import check_api_is_up, check_simulation_entries
 
 BASE_URL = "http://0.0.0.0:5001"
 
 
 dotenv_path = os.path.normpath(os.path.join(os.path.realpath(__file__), "../", ".env"))
 load_dotenv(dotenv_path)
-
-
-def api_is_up():
-    try:
-        response = requests.get(f"{BASE_URL}/")
-        return response.status_code == HTTPStatus.OK
-    except Exception:
-        return False
-
-
-async def influx_has_entries(client: InfluxDBClientAsync):
-    try:
-        results = await execute_influx_query(
-            client,
-            build_get_values_query(
-                5,
-                "heat_pipes",
-                "power",
-            ),
-        )
-        return len(results["_value"]) > 0
-    except Exception:
-        return False
-
-
-async def _check_simulation_entries():
-    async with get_influx_client() as client:
-        async with asyncio.timeout(20):
-            while True:
-                if await influx_has_entries(client):
-                    break
-                await asyncio.sleep(0.5)
-
-
-async def _check_api_is_up():
-    async with asyncio.timeout(20):
-        while True:
-            if api_is_up():
-                break
-            await asyncio.sleep(0.5)
 
 
 @pytest.fixture()
@@ -69,8 +20,8 @@ def headers():
 
 @pytest.fixture(scope="session", autouse=True)
 async def setup():
-    await _check_api_is_up()
-    await _check_simulation_entries()
+    await check_api_is_up()
+    await check_simulation_entries()
     yield
 
 
