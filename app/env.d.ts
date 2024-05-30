984cec89126a387974ae1f61eddad9de55ab631c
David Hoevers <david@manklavier.nl>
2024-05-30 08:29:52 +0200

    Use env variables for dev, prod and test (#121)

--- app/env.d.ts
+++ app/env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_MQTT: string;
+  readonly VITE_MQTT_USER: string;
+  readonly VITE_MQTT_PASSWORD: string;
+  readonly VITE_API: string;
+  readonly VITE_API_BEARER_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
