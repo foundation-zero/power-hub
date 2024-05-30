a10da7246c611b55b39dbf161023389970d75e7e
sietse-zero <127860207+sietse-zero@users.noreply.github.com>
2024-05-30 14:58:48 +0200

    Fix infra deployment (#125)

--- energy_box_control/config.py
+++ energy_box_control/config.py
@@ -5,16 +5,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class PowerHubConfig(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    influxdb_user: str
-    influxdb_pass: str
-    influxdb_organisation: str
-    influxdb_telegraf_bucket: str
-    influxdb_token: str
-    influxdb_url: str = Field(pattern="(https?)://([a-zA-Z0-9.-]+):(\\d+)")
-    mqtt_host: str = Field(pattern="^[^/:]+$")
-    mqtt_password: str
-    mqtt_username: str
-    api_token: str
+    influxdb_user: str = Field(default="")
+    influxdb_pass: str = Field(default="")
+    influxdb_organisation: str = Field(default="")
+    influxdb_telegraf_bucket: str = Field(default="")
+    influxdb_token: str = Field(default="")
+    influxdb_url: str = Field(
+        pattern="(https?)://([a-zA-Z0-9.-]+):(\\d+)", default="http://influxdb:8086"
+    )
+    mqtt_host: str = Field(pattern="^[^/:]+$", default=" ")
+    mqtt_password: str = Field(default="")
+    mqtt_username: str = Field(default="")
+    api_token: str = Field(default="")
     open_weather_api_key: str = Field(default="")
     logging_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = Field(
         default="INFO"
