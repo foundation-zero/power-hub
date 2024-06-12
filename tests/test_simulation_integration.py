90419a3a9f678530a40e542ac1ff61fe3c48c1e4
Sietse Huisman <sietse.huisman@blinkcv.com>
2024-06-12 11:45:44 +0200

    Add simulation endpoints integration test

--- tests/test_simulation_integration.py
+++ tests/test_simulation_integration.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 import json
 import os
+import aiohttp
 import pytest
 import requests
 from tests.test_api_integration import check_api_is_up, check_simulation_entries
@@ -47,3 +48,50 @@ def test_water_circuit(headers):
     )
     assert response.status_code == HTTPStatus.OK
     assert len(json.loads(response.text)) > 0
+
+
+async def assert_get_url(url: str, headers: dict[str, str]):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            assert response.status == HTTPStatus.OK
+            return json.loads(await response.text())
+
+
+@pytest.mark.integration
+async def test_display_endpoints(headers):
+    multiple_value_endpoints = [
+        "/power_hub/appliance_sensors/fresh_water_tank/fill/last_values",
+        "/power_hub/electric/power/production/over/time?interval=h",
+        "/power_hub/electric/power/consumption/over/time?interval=h",
+    ]
+    single_value_endpoints = [
+        "/power_hub/appliance_sensors/pv_panel/power/mean",
+        "/power_hub/electric/power/consumption/mean",
+        "/power_hub/appliance_sensors/heat_pipes/power/mean",
+        "/power_hub/appliance_sensors/chiller/chill_power/mean",
+        "/power_hub/appliance_sensors/yazaki/chill_power/mean",
+        "/power_hub/appliance_sensors/cold_reservoir/fill_power/mean",
+        "/power_hub/appliance_sensors/water_maker/out_flow/mean",
+        "/power_hub/appliance_sensors/water_treatment/out_flow/mean",
+        "/power_hub/appliance_sensors/fresh_water_tank/water_demand/mean",
+        "/power_hub/appliance_sensors/heat_pipes/output_temperature/mean",
+        "/power_hub/appliance_sensors/heat_pipes/input_temperature/mean",
+        "/power_hub/appliance_sensors/pcm/net_charge/mean",
+        "/power_hub/appliance_sensors/pcm/charge_power/mean",
+        "/power_hub/appliance_sensors/pcm/discharge_power/mean",
+        "/power_hub/appliance_sensors/pcm/temperature/mean",
+        "/power_hub/appliance_sensors/yazaki/chilled_output_temperature/mean",
+    ]
+
+    not_implemented_yet = [
+        "/power_hub/appliance_sensors/compound/overall_temperature/mean",
+        "/power_hub/weather_station/pyranometer/irradiance/mean",
+    ]
+
+    for endpoint in multiple_value_endpoints:
+        response = await assert_get_url(f"{BASE_URL}{endpoint}", headers)
+        assert len(response) > 0
+
+    for endpoint in single_value_endpoints:
+        response = await assert_get_url(f"{BASE_URL}{endpoint}", headers)
+        assert type(response) == float or int
