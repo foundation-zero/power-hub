44dc93e6689adb7e612b3eb4e37217007b4a375f
Sietse Huisman <sietse.huisman@blinkcv.com>
2024-06-19 16:53:01 +0200

    Process feedback

--- tests/test_api_integration.py
+++ tests/test_api_integration.py
@@ -164,10 +164,6 @@ async def test_get_current_pcm_fill(headers):
         >= 0
     )
 
-@pytest.mark.integration
-async def test_get_values_over_time(headers):
-    assert len(json.loads(await do_request(f"{BASE_URL}/power_hub/appliance_sensors/heat_pipes/power/over/time", headers=headers,))) > 0
-
 
 @pytest.mark.integration
 async def test_get_values_over_time(headers):
@@ -181,6 +177,18 @@ async def test_get_values_over_time(headers):
             )
         )
         > 0
+    )
+
+
+@pytest.mark.integration
+async def test_get_electric_power_consumption(headers):
+    assert len(
+        json.loads(
+            await do_request(
+                f"{BASE_URL}/power_hub/electric/power/consumption/over/time",
+                headers=headers,
+            )
+        )
     )
 
 
