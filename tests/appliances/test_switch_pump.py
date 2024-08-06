1834c2ebf0045fb4d0326b7e71cd0d83683650ca
sietse-zero <127860207+sietse-zero@users.noreply.github.com>
2024-08-06 09:34:54 +0200

    Change switch pump sensors according to IO (#231)
    * Change switch pump sensors according to IO
    
    * Process feedback

--- tests/appliances/test_switch_pump.py
+++ tests/appliances/test_switch_pump.py
@@ -8,6 +8,8 @@ from energy_box_control.appliances import (
     SwitchPumpControl,
 )
 from energy_box_control.appliances.base import ThermalState
+from energy_box_control.power_hub.network import PowerHub
+from energy_box_control.power_hub.schedules import PowerHubSchedules
 from energy_box_control.time import ProcessTime
 
 from energy_box_control.power_hub.components import SWITCH_PUMP_POWER
@@ -42,3 +44,12 @@ def test_switch_pump_on(switch_pump, simulation_time):
     )
     assert outputs[SwitchPumpPort.OUT].flow == 1
     assert outputs[SwitchPumpPort.OUT].temperature == 50
+
+
+def test_switch_pump_on_property():
+    power_hub = PowerHub.power_hub(PowerHubSchedules.const_schedules())
+    sensors = power_hub.sensors_from_state(power_hub.simple_initial_state())
+    sensors.pcm_to_yazaki_pump.status = 258  # 9th bit is set
+    assert sensors.pcm_to_yazaki_pump.on == True
+    sensors.pcm_to_yazaki_pump.status = 128  # 9th bit is not set
+    assert sensors.pcm_to_yazaki_pump.on == False
