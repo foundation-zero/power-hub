cf668dbd7962376c9204b60ef221c7760ecfa6ce
Boudewijn van Groos <boudewijn.vangroos@foundationzero.org>
2024-08-20 17:48:48 +0200

    Fix swipe buttons blocking start button (#273)

--- app/src/components/responsive/SwipeButtons.vue
+++ app/src/components/responsive/SwipeButtons.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="d-flex justify-space-between">
+  <div class="d-flex justify-space-between no-pointer">
     <SwipeButton
       direction="left"
+      class="with-pointer"
       :class="{ 'opacity-0': !swipeLeft, 'cursor-pointer': swipeLeft }"
       @click="swipeLeft && $emit('prev')"
     />
     <SwipeButton
       direction="right"
+      class="with-pointer"
       :class="{ 'opacity-0': !swipeRight, 'cursor-pointer': swipeRight }"
       @click="swipeRight && $emit('next')"
     />
@@ -24,5 +26,13 @@ defineEmits(["prev", "next"]);
 .swipe-button {
   transition: opacity 500ms ease;
   will-change: opacity;
+}
+
+.no-pointer {
+  pointer-events: none;
+}
+
+.with-pointer {
+  pointer-events: all;
 }
 </style>
