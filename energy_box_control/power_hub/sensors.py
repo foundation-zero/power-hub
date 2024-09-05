540e7dcd1e012b406e8a55614c54eac13db8b241
Alje <alje.vandam@foundationzero.org>
2024-09-05 20:09:16 +0200

    Process feedback

--- energy_box_control/power_hub/sensors.py
+++ energy_box_control/power_hub/sensors.py
@@ -833,7 +833,7 @@ class ChillerSensors(FromState):
         )
 
     @property
-    def waste_power(self) -> Watt:
+    def waste_heat(self) -> Watt:
         return (
             self.waste_flow
             * self.waste_delta_temperature
