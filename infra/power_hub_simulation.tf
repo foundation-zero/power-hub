15ab670122a3dd38044b7ee3161f2bab7ec39b21
Floris Smit <florissmit10@users.noreply.github.com>
2024-09-30 08:56:05 +0200

    Feature/improve build ci (#339)
    * Metadata of helm charts ignored by terraform
    
    * uncomment run condition
    
    * Split out staging prod & fix image tag

--- infra/power_hub_simulation.tf
+++ infra/power_hub_simulation.tf
@@ -55,4 +55,9 @@ resource "helm_release" "power_hub_simulation" {
     name  = "container.env.MQTT_HOST"
     value = kubernetes_service.vernemq_internal.metadata.0.name
   }
+  lifecycle {
+    ignore_changes = [
+      metadata
+    ]
+  }
 }
