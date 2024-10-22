eb30dfa80dc2bf22ff8fae8e68818d4f8423b0f2
Alje <alje.vandam@foundationzero.org>
2024-10-22 16:10:39 +0200

    Fix energy report and check kwh sensors

--- analysis/analysis_functions.py
+++ analysis/analysis_functions.py
@@ -9,18 +9,18 @@ import fluxy as fl
 from typing import Any
 
 def plot_hourly_avg(data: pd.DataFrame, ylabel: str):
-    hourly = data.set_index(data.index.hour)
-    hourly = hourly.stack().to_frame(name = ylabel).reset_index(names = ['hour', 'group'])
+    hourly = data.set_index(data.index.hour) # type: ignore
+    hourly = hourly.stack().to_frame(name = ylabel).reset_index(names = ['hour', 'group']) # type: ignore
 
     fig, ax = plt.subplots(dpi = 300)
     ax = sns.lineplot(data = hourly, x = 'hour', y= ylabel, hue = 'group', palette = 'Paired', dashes = False, linewidth = 2.0, ax = ax)
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.title(f'Hourly averages from {data.index[0].date()} to {data.index[-1].date()}')
+    plt.title(f'Hourly averages from {data.index[0].date()} to {data.index[-1].date()}') # type: ignore
 
 def plot_daily_bars(data: pd.DataFrame, ylabel: str):
 
     daily = data.resample('h').mean().resample('d').sum() 
-    daily.index =daily.index.date
+    daily.index =daily.index.date # type: ignore
     ax = daily.plot(kind = 'bar')
     ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
     ax.set_ylabel(ylabel)
@@ -73,3 +73,86 @@ kwh_meters_rename_dict = {**{f'electrical_e{group}_voltage_L1':f'electrical_e{gr
                           **{f'electrical_thermo_cabinet_voltage_L3':f'electrical_thermo_cabinet_power_L1'},
                           **{f'electrical_thermo_cabinet_current_L3':f'electrical_thermo_cabinet_power_L2'},
                           **{f'electrical_thermo_cabinet_power_L3':f'electrical_thermo_cabinet_power_L3'}}
+
+from itertools import product
+
+grid_power = [f"electrical_vebus_e{phase}_input_power" for phase in range(1,4)]
+AC_power = [f"electrical_vebus_e{phase}_output_power" for phase in range(1,4)]
+compound_variables = [f'electrical_e{container}_{var}_{phase}' for container,var,phase in product(range(1,9),["voltage","current", "power"],["L1","L2","L3"])]
+hub_variables = [f'electrical_thermo_cabinet_{var}_{phase}' for var,phase in product(["voltage","current", "power"],["L1","L2","L3"])]
+
+electric_variables = [
+    "electrical_pv_power",
+    *grid_power,
+    *AC_power,
+    *compound_variables,
+    *hub_variables,
+    "electrical_battery_system_soc"
+]
+
+electrical_groups = {
+    "pv_power": ["electrical_pv_power"],
+    "grid_power": [
+        "electrical_vebus_e1_input_power",
+        "electrical_vebus_e2_input_power",
+        "electrical_vebus_e3_input_power",
+    ],
+    "AC_power": ["electrical_vebus_e1_output_power",
+        "electrical_vebus_e2_output_power",
+        "electrical_vebus_e3_output_power"]}
+
+electrical_consumption_groups = {
+    "office": [
+        "electrical_e1_power_L1",
+        "electrical_e1_power_L2",
+        "electrical_e1_power_L3",
+    ],
+    "workshop": [
+        "electrical_e2_power_L1",
+        "electrical_e2_power_L2",
+        "electrical_e2_power_L3",
+    ],
+    "simulator": [
+        "electrical_e3_power_L1",
+        "electrical_e3_power_L2",
+        "electrical_e3_power_L3",
+    ],
+    "kitchen_sanitary_1": [
+        "electrical_e4_power_L1",
+        "electrical_e4_power_L2",
+        "electrical_e4_power_L3",
+    ],
+    "kitchen_sanitary_2": [
+        "electrical_e5_power_L1",
+        "electrical_e5_power_L2",
+        "electrical_e5_power_L3",
+    ],
+    "supply_box": [
+        "electrical_e6_power_L1",
+        "electrical_e6_power_L2",
+        "electrical_e6_power_L3",
+    ],
+    "center_1": [
+        "electrical_e7_power_L1",
+        "electrical_e7_power_L2",
+        "electrical_e7_power_L3",
+    ],
+    "center_2": [
+        "electrical_e8_power_L1",
+        "electrical_e8_power_L2",
+        "electrical_e8_power_L3",
+    ],
+    "power_hub": [
+        "electrical_thermo_cabinet_power_L1",
+        "electrical_thermo_cabinet_power_L2",
+        "electrical_thermo_cabinet_power_L3",
+    ],
+}
+
+electrical_groups_dict_inv = {
+    i: k for k in electrical_groups for i in electrical_groups[k]
+}
+
+electrical_consumption_groups_dict_inv = {
+    i: k for k in electrical_consumption_groups for i in electrical_consumption_groups[k]
+}
